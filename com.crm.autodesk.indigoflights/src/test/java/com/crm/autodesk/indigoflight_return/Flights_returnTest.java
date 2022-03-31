7c50c744f8891d0a15a8cfdeea05ffe468bd1893
CHAVAN <CHAVAN@KALYAN>
2022-03-31 14:51:16 +0530

    this is kailas review .

---
+++ com.crm.autodesk.indigoflights/src/test/java/com/crm/autodesk/indigoflight_return/Flights_returnTest.java
@@ -0,0 +1,15 @@
+package com.crm.autodesk.indigoflight_return;
+
+import org.testng.annotations.Test;
+
+public class Flights_returnTest {
+
+	@Test
+	public void returnTest() {
+		System.out.println("arrieval");
+		System.out.println("arrieval time");
+		System.out.println("destination");
+		System.out.println("destination time");
+		
+	}
+}
