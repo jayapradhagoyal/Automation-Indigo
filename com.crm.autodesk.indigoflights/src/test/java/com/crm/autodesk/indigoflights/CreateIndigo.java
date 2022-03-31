59b51ebfcc9ae2b4e2624f5f900454403e197555
zuser <zuser@DESKTOP-V2AP8VU>
2022-03-31 13:58:29 +0530

    commit 1

---
+++ com.crm.autodesk.indigoflights/src/test/java/com/crm/autodesk/indigoflights/CreateIndigo.java
@@ -0,0 +1,12 @@
+package com.crm.autodesk.indigoflights;
+
+import org.testng.annotations.Test;
+
+public class CreateIndigo {
+	@Test
+	public void createIndigoflights()
+	{
+		System.out.println("flights data has been ");
+	}
+
+}
